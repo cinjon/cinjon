eda74b1c6454cbcc4dd6fb3f4e588b4c013d597c
cinjon <cinjon@alum.mit.edu>
2013-11-20 01:32:50 -0800

    new text files / photo

--- cinjon.js
+++ cinjon.js
@@ -1,5 +1,16 @@
 if (Meteor.isClient) {
+    Meteor.autorun(function() {
+        Template.display.rendered = function() {
+            var offset = $("#main").offset();
+            $("#contacts").offset({left:offset.left});
+        },
 
+        Template.critique.subject = function() {
+            var str = "mailto:cinjon.resnick@gmail.com?subject="
+            var activeLI = $("li.active");
+            return str + activeLI.children().text();
+        };
+    });
 }
 
 if (Meteor.isServer) {
