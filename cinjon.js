27b7c2e3199a5d99fb943a400b18adc3644b2b24
cinjon <cinjon.resnick@gmail.com>
2013-12-26 20:00:21 -0800

    spacing fix + update to projects

--- cinjon.js
+++ cinjon.js
@@ -1,15 +1,25 @@
 if (Meteor.isClient) {
+    function positionContacts() {
+        var offset = $("#main").offset();
+        $("#contacts").offset({left:offset.left});
+    }
+
     Meteor.autorun(function() {
         Template.display.rendered = function() {
-            var offset = $("#main").offset();
-            $("#contacts").offset({left:offset.left});
+            console.log('rendering display');
+            positionContacts();
         },
 
         Template.critique.subject = function() {
             var str = "mailto:cinjon.resnick@gmail.com?subject="
             var activeLI = $("li.active");
             return str + activeLI.children().text();
-        };
+        },
+
+        window.onresize = function(event) {
+            console.log('window resizing');
+            positionContacts();
+        }
     });
 }
 
@@ -18,3 +28,6 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+
+
