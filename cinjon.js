29b3723d35b820f67fdb805e6f7046d84a7910f2
cinjon <cinjon.resnick@gmail.com>
2014-01-09 11:22:11 -0500

    redesign

--- cinjon.js
+++ cinjon.js
@@ -1,13 +1,68 @@
 if (Meteor.isClient) {
-    function positionContacts() {
-        var offset = $("#main").offset();
-        $("#contacts").offset({left:offset.left});
+    function hideUL() {
+        $('.ulNav').hide();
+    }
+
+    function displayUL(ty) {
+        $('#fakeUL').hide();
+        $('#' + ty).show();
     }
 
     Meteor.autorun(function() {
         Template.display.rendered = function() {
-            positionContacts();
-            changeNavToType();
+            hideUL();
+        },
+
+        Template.welcome.rendered = function() {
+            $("#inches").hover(function() {
+                $('#hoverText').html('Within that inch, I am free');
+                $('#hoverText').show();
+            }, function() {
+                $('#hoverText').hide();
+            });
+
+            $("#contact").hover(function() {
+                $(this).css('cursor','pointer');
+                $('#hoverText').html('Click to email me');
+                $('#hoverText').show();
+            }, function() {
+                $('#hoverText').hide();
+                $(this).css('cursor', 'default');
+            });
+
+            $("#projects").hover(function() {
+                $(this).css('cursor','pointer');
+                $('#hoverText').html('See LinkedIn for professional work');
+                $('#hoverText').show();
+                //show something below
+            }, function() {
+                $('#hoverText').hide();
+                $(this).css('cursor', 'default');
+            });
+
+            $("#writing").hover(function() {
+                $(this).css('cursor','pointer');
+                $('#hoverText').html('Most everything here always needs more editing');
+                $('#hoverText').show();
+                //show something below
+            }, function() {
+                $('#hoverText').hide();
+                $(this).css('cursor', 'default');
+            });
+
+            $('#projects').click(function() {
+                hideUL();
+                displayUL('projectUL');
+            });
+
+            $('#writing').click(function() {
+                hideUL();
+                displayUL('writingUL');
+            });
+
+            $('#contact').click(function() {
+                window.location.href = "mailto:cinjon.resnick@gmail.com?subject=Thanks for the message";
+            });
         },
 
         Template.critique.subject = function() {
@@ -18,11 +73,6 @@ if (Meteor.isClient) {
 
         window.onresize = function(event) {
             positionContacts();
-        },
-
-        changeNavToType = function(ty) {
-            $('li').not('.active').not('.' + ty).hide();
-            $('li.' + ty).show();
         }
     });
 }
